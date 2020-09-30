2a35786c465f757126a5ddf3bbdbb0f609a79512
satojun3 <34908655+satojun3@users.noreply.github.com>
2020-10-01 08:17:12 +0900

    Using package (strings, os, bufio )

--- english-vocabulary-problem/english_vocabulary_program.go
+++ english-vocabulary-problem/english_vocabulary_program.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -15,11 +18,25 @@ func main() {
 	// fmt.Println(ip)  // pointer型を表示
 	// fmt.Println(*ip) // pointerに格納されている値を表示
 
-	var mode *string = flag.String("mode", "typing", "This is a written test of English words.")
+	var mode *string = flag.String("mode", "w", "This is a written test of English words.")
 	flag.Parse()
-	if *mode == "typing" {
+	if *mode == "w" {
 		fmt.Printf("This is a written test of English words.\n")
-	} else if *mode == "optional" {
+
+		japaneseWord := "リンゴ"
+		englishWord := "apple"
+		fmt.Println(japaneseWord + "を英語にしなさい。")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		// Removing blank spaces
+		i := strings.TrimSpace(scanner.Text())
+
+		if i == englishWord {
+			fmt.Println("正解")
+		} else {
+			fmt.Println("不正解、正解は" + englishWord + "です。")
+		}
+	} else if *mode == "s" {
 		fmt.Printf("This is a selective test.\n")
 	}
 }
