c16c67e360417691026d2625b29c96e61f29431a
satojun3 <34908655+satojun3@users.noreply.github.com>
2020-09-30 20:08:10 +0900

    Introducing the flag package

--- english-vocabulary-problem/english_vocabulary_program.go
+++ english-vocabulary-problem/english_vocabulary_program.go
@@ -6,7 +6,20 @@ import (
 )
 
 func main() {
+	// flag.Parse()
+	// args := flag.Args()
+	// fmt.Println(args)
+
+	// var ip *int = flag.Int("int", 1234, "help message for flagname")
+	// flag.Parse()     // flag定義をした後にParseを実行する
+	// fmt.Println(ip)  // pointer型を表示
+	// fmt.Println(*ip) // pointerに格納されている値を表示
+
+	var mode *string = flag.String("mode", "typing", "This is a written test of English words.")
 	flag.Parse()
-	args := flag.Args()
-	fmt.Println(args)
+	if *mode == "typing" {
+		fmt.Printf("This is a written test of English words.\n")
+	} else if *mode == "optional" {
+		fmt.Printf("This is a selective test.\n")
+	}
 }
