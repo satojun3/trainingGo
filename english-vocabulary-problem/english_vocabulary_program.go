e464ff1e3807036fa24892288639c681886c3db1
satojun3 <34908655+satojun3@users.noreply.github.com>
2020-10-04 23:38:52 +0900

    Coding an english-vocabulary-program.

--- english-vocabulary-problem/english_vocabulary_program.go
+++ english-vocabulary-problem/english_vocabulary_program.go
@@ -5,38 +5,52 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+	// "strings"
 )
 
 func main() {
-	// flag.Parse()
-	// args := flag.Args()
-	// fmt.Println(args)
-
-	// var ip *int = flag.Int("int", 1234, "help message for flagname")
-	// flag.Parse()     // flag定義をした後にParseを実行する
-	// fmt.Println(ip)  // pointer型を表示
-	// fmt.Println(*ip) // pointerに格納されている値を表示
-
+	// オプションの設定 初期値は w
 	var mode *string = flag.String("mode", "w", "This is a written test of English words.")
+	// フラグの取得
 	flag.Parse()
-	if *mode == "w" {
-		fmt.Printf("This is a written test of English words.\n")
 
-		japaneseWord := "リンゴ"
-		englishWord := "apple"
-		fmt.Println(japaneseWord + "を英語にしなさい。")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		// Removing blank spaces
-		i := strings.TrimSpace(scanner.Text())
-
-		if i == englishWord {
-			fmt.Println("正解")
-		} else {
-			fmt.Println("不正解、正解は" + englishWord + "です。")
-		}
-	} else if *mode == "s" {
-		fmt.Printf("This is a selective test.\n")
+	// 分岐
+	switch *mode {
+	case "w":
+		fmt.Printf("記述式の英単語プログラムです。\n")
+	case "s":
+		fmt.Printf("選択式のプログラムです。\n")
+	default:
+		fmt.Printf("オプションを間違えています。w か s を選択してください。")
 	}
+
+	fmt.Printf(inputAnswer("aaa"))
+
+	// testScanner := bufio.NewScanner(os.Stdin)
+	// bool := testScanner.Scan()
+	// fmt.Println(bool)
+	// for testScanner.Scan() {
+	// 	if testScanner.Text() == "stop" {
+	// 		break
+	// 	}
+	// 	fmt.Println("It's on an infinite loop.")
+	// }
+	// fmt.Println("end.")
+}
+
+func inputAnswer(correct string) string {
+	var responseWord string = ""
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == correct {
+			responseWord = "正解です。\n"
+			break
+		} else if scanner.Text() == "" {
+			responseWord = "パスします。\n"
+			break
+		}
+		fmt.Printf("不正解です。\n")
+	}
+	return responseWord
 }
