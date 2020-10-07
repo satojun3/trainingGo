6608918cd35f3e7a0e569dd6ab1fc2a66d5b6f28
satojun3 <34908655+satojun3@users.noreply.github.com>
2020-10-07 21:23:16 +0900

    structure sample

--- tip-on-structures/structures.go
+++ tip-on-structures/structures.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Person is sample. Definition of a basic structure
@@ -13,11 +14,47 @@ type Person struct {
 }
 
 func main() {
+	// ケース1
 	hoge := Person{
-		Age:        65,
 		FirstName:  "Taro",
 		SecondName: "Yamada",
 		Sex:        "men",
+		Age:        65,
 	}
-	fmt.Println(hoge)
+	fmt.Println("Name：" + hoge.FirstName + " " + hoge.SecondName)
+	fmt.Println("Age:" + strconv.Itoa(hoge.Age))
+	fmt.Println("Sex:" + hoge.Sex)
+	// 型を調べる
+	fmt.Printf("%T\n", hoge)
+	fmt.Println(&hoge)
+
+	fmt.Printf("\n")
+
+	// ケース2
+	hoge2 := new(Person)
+	hoge2.FirstName = "Hanako"
+	// hoge2.SecondName = "Tanaka" // 代入をしなくても初期値が入っている => ""
+	// hoge2.Age = 65              // 代入をしなくても初期値が入っている => 0
+	hoge2.Sex = "female"
+	fmt.Println("Name：" + hoge2.FirstName + " " + hoge2.SecondName)
+	fmt.Println("Age:" + strconv.Itoa(hoge2.Age))
+	fmt.Println("Sex:" + hoge2.Sex)
+	// 型を調べる
+	fmt.Printf("%T\n", hoge2)
+	fmt.Println(&hoge2)
+
+	fmt.Printf("\n")
+
+	// ケース3
+	hogeGroup := make([]Person, 2)
+	hogeGroup[0] = hoge
+	hogeGroup[1] = *hoge2
+	for _, v := range hogeGroup {
+		fmt.Println("Name：" + v.FirstName + " " + v.SecondName)
+		fmt.Println("Age:" + strconv.Itoa(v.Age))
+		fmt.Println("Sex:" + v.Sex)
+		// 型を調べる
+		fmt.Printf("%T\n", v)
+		fmt.Println(&v)
+	}
 }
